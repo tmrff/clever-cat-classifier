9c20844393e257b842a4f11e84f80a583b0d4768
Thomas <thomas.roff@bluejeans.com>
2022-02-27 12:01:45 +1300

    Conform to swiftlint

--- CleverCatClassifier/CleverCatClassifier/LiveImageViewController.swift
+++ CleverCatClassifier/CleverCatClassifier/LiveImageViewController.swift
@@ -15,7 +15,6 @@ class LiveImageViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
     /*
     // MARK: - Navigation
 
--- CleverCatClassifier/CleverCatClassifier/StillImageViewController.swift
+++ CleverCatClassifier/CleverCatClassifier/StillImageViewController.swift
@@ -15,7 +15,6 @@ class StillImageViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
     /*
     // MARK: - Navigation
 
